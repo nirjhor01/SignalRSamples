518f49273b701eec22e71f14281bc3e8c7dd50d3
Nirjhor <tanveer.ahmed@technonext.com>
2024-05-21 16:00:02 +0600

    Commited

---
+++ SignalRSamples/Hubs/UserHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSamples.Hubs
+{
+    public class UserHub : Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+        public static int TotalUsers { get; set; } = 0;
+        public override Task OnConnectedAsync()
+        {
+            TotalUsers += 1;
+            Clients.All.SendAsync("updateTotalUsers",TotalUsers).GetAwaiter().GetResult();
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUsers -= 1;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnDisconnectedAsync(exception);
+        }
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++; 
+            await Clients.All.SendAsync("UpdateTotalViews", TotalViews);
+        }
+    }
+}
+ 
---
+++ SignalRSamples/wwwroot/js/UserCount.js
@@ -0,0 +1,28 @@
+﻿//Create Connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("hubs/userCount").build();
+//methods that hub invokes aka receive notifications from hub.
+connectionUserCount.on("UpdateTotalViews", (value) => {
+    console.log(value);
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+
+});
+connectionUserCount.on("updateTotalUsers", (val) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = val.toString();
+});
+//3-invoke hub methods aka send notifications to hub.
+function NewWindowLoadedOnClient() { 
+    connectionUserCount.send("NewWindowLoaded");
+}
+//Start connection
+function fulfilled() {
+    NewWindowLoadedOnClient();
+    console.log("Connection to Uer Hub Successful");
+}
+
+function rejected() {
+    //rejected logs
+     
+}
+connectionUserCount.start().then(fulfilled, rejected);
